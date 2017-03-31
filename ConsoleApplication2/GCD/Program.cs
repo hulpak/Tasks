977f28a0af085ea901ebef82522725c4ed11307e
ronaldo7 <gulpak@bk.ru>
2017-03-31 16:17:22 +0300

    [+] GCD

---
+++ ConsoleApplication2/GCD/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string[] arr = line.Split();
+            var first = arr.First().ToString();
+            int[] number = Array.ConvertAll(arr, int.Parse).ToArray();
+
+            Console.WriteLine(GCD(number[0],number[1]));
+            Console.ReadKey();
+        }
+        static int GCD(int a, int b)
+        {
+            if (a == 0 || b == 0) return a + b; 
+            return GCD(b, a % b);
+        }
+    }
+}
