f444494c2e1db702c985b56a55216456dd4cf3fe
gulpak@bk.ru <ronaldo7>
2017-04-02 18:55:18 +0300

    [+]array

--- ConsoleApplication2/SortArray/SortArray.cs
+++ ConsoleApplication2/SortArray/SortArray.cs
@@ -12,8 +12,8 @@ namespace SortArray
         {
             string line = Console.ReadLine();
             string[] arr = line.Split();
-            var first = arr.First().ToString();
-            int[] number = Array.ConvertAll(arr, int.Parse).Skip(1).OrderBy(i=>i).ToArray();
+            int first = int.Parse(arr.First().ToString());
+            int[] number = Array.ConvertAll(arr, int.Parse).Skip(1).Take(first).OrderBy(i=>i).ToArray();
 
             Console.Write(first + " ");
             foreach (int i in number)
